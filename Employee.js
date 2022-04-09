83a9db294e9b198ca2e1bb25b06975ba86d844d6
Shogun <shogunzaku@gmail.com>
2022-04-09 13:56:18 -0700

    tracking the Employee.js file,added my employee constructor and module export.

---
+++ Employee.js
@@ -0,0 +1,27 @@
+// Lets Create the Constructor for the employees,Objects.
+class Employee {
+    constructor(name, id, email,) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+
+    }
+    getName() {
+        return this.name;
+
+    }
+
+    getId() {
+        return this.id;
+
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Employee";
+    }
+}
+module.exports = Employee;
