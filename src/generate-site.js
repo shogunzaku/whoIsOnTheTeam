8c191a0b8edd4b6af292b67a7fff0a4a56159037
Shogun <shogunzaku@gmail.com>
2022-04-13 00:53:12 -0700

    created generate engineer html info

--- src/generate-site.js
+++ src/generate-site.js
@@ -1,11 +1,11 @@
 const generateTeam = (team) => {
     console.log(team);
-// Creates a empty array to push html elements on to loop through the team data
+    // Creates a empty array to push html elements on to loop through the team data
     const html = [];
     // Creates functions for each type of employee that returns HTML data
     const generateManager = manager => {
         console.log(manager);
-        let managerHtmll = `
+        let managerHtml = `
         <div class="card" style="width: 18rem;">
         <div class="card-header">
        ${manager.name} <br/>
@@ -17,5 +17,21 @@ const generateTeam = (team) => {
         </ul>
     </div>
         `;
-    html.push(managerHtml);
+        html.push(managerHtml);
+    }
+    const generateEngineer = engineer => {
+        console.log(engineer);
+        let engineerHtml = `
+        <div class="card" style="width: 18rem;">
+        <div class="card-header">
+        ${engineer.name} <br/>
+       <i class="fas fa-glasses"></i>Engineer</div>
+       <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineer.id}</li>
+        <li class="list-group-item">Email: <span id="email"><a href="mailto${engineer.email}">${engineer.email}</a></span></li>
+        <li class="list-group-item">Github Username: <a target="_blank" href="https://github.com/${engineer.githubUsername}">${engineer.githubUsername}</a></li>
+        </ul>
+    </div>
+        `;
+    html.push(engineerHtml);
     }
